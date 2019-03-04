3d9c884a8dd87caff41126a731019eeb5f82395c
Alfredo Campelo <egofrido@gmail.com>
2019-03-04 23:52:54 +0100

    añadido buscarR()

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -8,11 +8,35 @@ console.log("Route to node.js: " + rutaNode);
 //imprime en pantalla ruta al fichero
 console.log("Route to this file: " + rutaFichero);
 
-//busca mientras exista "-r" en el array
-while(parametros.find(elem => elem === "-r")){
-	let indiceR = parametros.findIndex(elem => elem === "-r");
-	console.log("indiceR = " + indiceR);
-	//elimina "-r" y el parametro que lo sigue
-	parametros.splice(indiceR, 2);
-}
+parametros.forEach((val, index) => {
+	console.log(`${index}: ${val}`);
+});/**/
 
+function buscarR(){
+//busca mientras exista "-r" en el array
+	while(parametros.find(elem => elem === "-r")){
+		//si existe el parametro -r inicializa la variable con el indice
+		let indiceR = parametros.findIndex(elem => elem === "-r");
+		//inicializa la variable con el parametro a continuacion de -r
+		let paramDel = parametros[indiceR + 1];
+		console.log("indiceR = " + indiceR);
+		console.log("paramDel = " + paramDel);
+		//elimina el parametro -r del array
+		parametros.splice(indiceR, 1);
+		//recorre el array buscando el parametro a eliminar
+		for(let i in parametros){
+			console.log("parametros[" + i + "] = " + parametros[i]);
+			if(parametros[i] === paramDel){
+				console.log("en el if " + parametros[i]);
+				parametros.splice(i, 1);
+			}/**/
+		}
+	}
+}
+buscarR();
+//ordena el array
+parametros.sort();
+
+parametros.forEach((val, index) => {
+	console.log(`${index}: ${val}`);
+});
