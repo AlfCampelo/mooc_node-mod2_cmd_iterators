3539d406b6e8bcf1da3dc4546dd65e81e0342beb
Alfredo Campelo <egofrido@gmail.com>
2019-03-04 14:57:05 +0100

    multiasignación de variables

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -1,1 +1,11 @@
-// Type your code here
+//Linea en blanco
+console.log();
+
+//declaración y multiasignación de variables
+let [rutaNode, rutaFichero, parametros] = [process.argv[0], process.argv[1], process.argv.slice(2)];
+//imprime en pantalla ruta a node.js 
+console.log("Route to node.js: " + rutaNode); 
+//imprime en pantalla ruta al fichero
+console.log("Route to this file: " + rutaFichero);
+
+
