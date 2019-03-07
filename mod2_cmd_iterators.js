774326488b1009d60c1840969fbf0fd047fc81a8
Alfredo Campelo <egofrido@gmail.com>
2019-03-07 14:44:25 +0100

    limpieza código

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -1,3 +1,5 @@
+//-------------------DECLARACIÓN DE FUNCIONES--------------------------------
+
 //Busca el parametro -r, y si existe borra el valor a continuación y todos los que son iguales 
 function buscarR(){
 //busca mientras exista "-r" en el array
@@ -6,24 +8,42 @@ function buscarR(){
 		let indiceR = parametros.findIndex(elem => elem === "-r");
 		//inicializa la variable con el parametro a continuacion de -r
 		let paramDel = parametros[indiceR + 1];
-
-		console.log("indiceR = " + indiceR);
-		console.log("paramDel = " + paramDel);
-		console.log();
 		//elimina el parametro -r del array
 		parametros.splice(indiceR, 1);
 
 		//recorre el array buscando el parametro a eliminar
 		for(let i = 0; i < parametros.length; i++){
-			console.log("parametros[" + i + "] = " + parametros[i]);
 			if(parametros[i] === paramDel){
-				console.log("en el if, borra " + parametros[i]);
 				parametros.splice(i, 1);
 				--i;
-			}/**/
+			}
 		}
 	}
 }
+
+//recorre el array contando el número de parametros iguales
+// y mostrandolos en pantalla
+function reducir(){	
+	let cont = 1;//inicializa contador de parametros	 
+	parametros.reduce((acc, actual, i, a) => 
+		{	//compara el valor actual con el anterior
+			//si son iguales incrementa el contador.
+			if(actual === a[i-1]){			 	
+				cont++;
+			//si no son iguales muestra el parametro y el nº de veces contenido en el array
+			// e inicializa el contador.
+			}else{
+				console.log(`${a[i-1]}: ${cont}`);
+				cont = 1;
+			}
+			//si es el último valor lo muestra en pantalla
+			if(i === a.length - 1){
+				console.log(`${actual}: ${cont}`);
+			}
+		});
+}
+
+//----------------------FIN DECLARACIÓN DE FUNCIONES---------------------------------
 
 
 //Linea en blanco
@@ -33,59 +53,20 @@ console.log();
 let [rutaNode, rutaFichero, parametros] = [process.argv[0], process.argv[1], process.argv.slice(2)];
 
 //imprime en pantalla ruta a node.js 
-console.log("Route to node.js: " + rutaNode); 
+console.log(`Route to node.js: ${rutaNode}`); 
 
 //imprime en pantalla ruta al fichero
-console.log("Route to this file: " + rutaFichero);
-
-parametros.forEach((val, index) => {
-	console.log(`${index}: ${val}`);
-});/**/
+console.log(`Route to this file: ${rutaFichero}`);
 
 //Linea en blanco
 console.log();
 
 //Busca el parametro -r
 buscarR();
-console.log();
-console.log("antes de ordenar");
-parametros.forEach((val, index) => {
-	console.log(`${index}: ${val}`);
-});
+
 //ordena el array
 parametros.sort();
 
-console.log(parametros);
-
-console.log("despues de ordenar");
-parametros.forEach((val, index) => {
-	console.log(`${index}: ${val}`);
-});
-console.log();
-console.log("antes de reducir");
-
-//recorre el array contando el número de parametros iguales
-// y mostrandolos en pantalla
-function reducir(){	
-	let cont = 1;//inicializa contador de parametros	 
-	parametros.reduce((acc, actual, i, a) => 
-		{	
-			if(actual === a[i-1]){			 	
-				cont++;
-			}else{
-				console.log(a[i-1] +":" + cont);
-				cont = 1;
-			}
-			if(i === a.length - 1){
-				console.log(actual + ":" + cont);
-			}
-		});
-}
-
+//cuenta el número de parametros iguales y los muestra
+//en pantalla
 reducir();
-/*
-
-console.log();
-parametros.forEach((val, index) => {
-	console.log(`${index}: ${val}`);
-});*/
